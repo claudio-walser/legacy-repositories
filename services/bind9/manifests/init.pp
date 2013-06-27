151e64a778a09b71d30c9f8925973743e63607e8
Claudio Walser <claudio@claudio.dev>
2013-06-27 21:45:01 +0200

    master conf of course

--- services/bind9/manifests/init.pp
+++ services/bind9/manifests/init.pp
@@ -6,7 +6,15 @@ class bind9 {
         group => bind,
         source => "puppet:///modules/bind9/master/var/lib/bind/claudio.dev.hosts"
     }
-    
+
+    file { "named.conf.local":
+        path => "/etc/bind/named.conf.local",
+        mode => 655,
+        owner => bind,
+        group => bind,
+        source => "puppet:///modules/bind9/master/etc/bind/named.conf.local"
+    }
+
     package { "bind9": 
         ensure => installed 
     }
