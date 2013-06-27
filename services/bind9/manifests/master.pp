0afb8602a460abab775fe4536ca30cfb51b05030
Claudio Walser <claudio@claudio.dev>
2013-06-27 23:17:16 +0200

    Cleaned up, moved service restart to end, changed file resources a bit

--- services/bind9/manifests/master.pp
+++ services/bind9/manifests/master.pp
@@ -3,7 +3,7 @@ class bind9::master {
     file { "claudio.dev.hosts":
         ensure  => file,
         path => "/var/lib/bind/claudio.dev.hosts",
-        mode => 0644
+        mode => 0644,
         owner => bind,
         group => bind,
         source => "puppet:///modules/bind9/master/var/lib/bind/claudio.dev.hosts"
