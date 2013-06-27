e445eded26098e3738350d5270de9a2ef63108d8
Claudio Walser <claudio@claudio.dev>
2013-06-27 13:02:29 +0200

    user::virtual not working yet, go on with something easier like motd :p

--- modules/common/manifests/init.pp
+++ modules/common/manifests/init.pp
@@ -1,6 +1,23 @@
 class common {
 	## common users
-	include user::virtual
+	##include user::virtual
+
+	file {'motd':
+		ensure  => file,
+		path    => '/etc/motd',
+		mode    => 0644,
+		content => 
+			"This Learning Puppet VM's IP address is ${ipaddress}. It thinks its
+			hostname is ${fqdn}, but you might not be able to reach it there
+			from your host machine. It is running ${operatingsystem} ${operatingsystemrelease} and
+			Puppet ${puppetversion}.
+			Web console login:
+			URL: https://${ipaddress_eth0}
+			User: puppet@example.com
+			Password: learningpuppet"
+	}
+
+
 
     package { "ssh": 
         ensure => installed 
