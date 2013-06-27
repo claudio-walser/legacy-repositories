7ec3fc311166a2ed09294eac3129de9926fb0628
Claudio Walser <claudio@claudio.dev>
2013-06-27 16:39:42 +0200

    Added nsupdate for nameservers

--- services/bind9/manifests/init.pp
+++ services/bind9/manifests/init.pp
@@ -1,5 +1,12 @@
 class bind9 {
 
+	file { "/var/cache/bind/claudio.dev.hosts":
+	    #mode => 440,
+	    #owner => root,
+	    #group => root,
+	    source => "puppet:///modules/bind9/claudio.dev.hosts"
+	}
+
     package { "bind9": 
         ensure => installed 
     }
@@ -8,10 +15,9 @@ class bind9 {
         ensure => running
     }
 
-    file { "/var/cache/bind/claudio.dev.hosts":
-	    #mode => 440,
-	    #owner => root,
-	    #group => root,
-	    source => "puppet:///modules/bind9/claudio.dev.hosts"
-	}
+    package { "nsupdate": 
+        ensure => installed 
+    }
+
+
 }
