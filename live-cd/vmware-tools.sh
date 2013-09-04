0fa9c60e749e778766dbf2da49ad494744e18d9c
Claudio Walser <claudio.walser@srf.ch>
2013-09-04 19:39:23 +0200

    Not sure if this mess came from a build, dont know what exactly happend

--- live-cd/vmware-tools.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-# has to be installed in the live-cd to mount shared host folders
-# this is important to provision the machine initially by setting a full qualified hostname
-echo 'install vmware-tools'
