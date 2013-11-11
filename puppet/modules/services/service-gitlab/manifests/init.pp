68b234666fc5099ce25d7c8167f0dac758470f47
Claudio Walser <claudio.walser@srf.ch>
2013-11-12 00:50:10 +0100

    Gitlab works well now on debian wheezy

--- puppet/modules/services/service-gitlab/manifests/init.pp
+++ puppet/modules/services/service-gitlab/manifests/init.pp
@@ -1,66 +1,44 @@
-class service-gitlab {
+class service-gitlab (
+    db_name = 'gitlab',
+    db_user = 'gitlab',
+    db_pass = 'gitlab'
+) { 
 
-    $fixed_ip = get_host_ip($network)
+    # prerequisites
+    #class { '::nginx': } ->
+    
+    package { [
+        'g++',
+        'nginx',
+        'ruby1.9.3',
+        # redis puppet modules are a nightmare, at least with librarian-puppet
+        'redis-server'
+    ]:
+        ensure => 'installed'
+    }
+    
+    # install mysql-server with root password icinga
+    class { '::mysql::server': }
 
-    # dependencies
-    package {
-        ['build-essential',
-            'zlib1g-dev',
-            'libyaml-dev',
-            'libssl-dev',
-            'libgdbm-dev',
-            'libreadline-dev',
-            'libncurses5-dev',
-            'libffi-dev',
-            'curl',
-            'git-core',
-            'redis-server',
-            'checkinstall',
-            'libxml2-dev',
-            'libxslt-dev',
-            'libcurl4-openssl-dev',
-            'libicu-dev',
-            'python2.7',
-            'ruby'
-            ]: 
-        ensure => installed 
-    } ->
-
-    # gitlab
-    file { '/etc/apt/sources.list.d/gitlab.list':
-        owner => 'root',
-        group => 'root',
-        ensure => 'file',
-        content =>  "deb http://debian.fastlaneventures.ru/gitlab wheezy/\ndeb-src http://debian.fastlaneventures.ru/gitlab wheezy/"
-    } ->
-
-    exec { 'git-apt-update':
-        command => "apt-get update",
-        path => '/usr/bin'
-    } ->
-
-    exec { 'git-apt-install':
-        command => "apt-get --force-yes install gitlab",
-        path => '/usr/bin'
-    } ->
-
-
-    # default motd
-    file {'/etc/gitlab/unicorn.rb':
-        ensure  => file,
-        mode    => 0644,
-        content => template("service-gitlab/etc/gitlab/unicorn.rb.erb")
-    } ->
-
-    file {'/etc/gitlab/gitlab.yml':
-        ensure  => file,
-        mode    => 0644,
-        content => template("service-gitlab/etc/gitlab/gitlab.yml.erb")
-    }->
-
-    service { "gitlab":
-        enable => true,
-        ensure => running
+    mysql::db { $db_name: 
+        user => $db_user,
+        password => $db_pass,
+        grant => 'ALL',
+        charset => 'utf8',
+        collate => 'utf8_general_ci',
+        host => 'localhost',
+        enforce_sql => false,
+        ensure => 'present'
     }
 
+    class { '::gitlab':
+        git_email         => 'cwa@uwd.ch',
+        git_comment       => 'GitLab',
+        gitlab_domain     => $fqdn,
+        gitlab_dbtype     => 'mysql',
+        gitlab_dbname     => $db_name,
+        gitlab_dbuser     => $db_user,
+        gitlab_dbpwd      => $db_pass,
+        require           => Package['g++', 'nginx', 'ruby1.9.3', 'redis-server']
+    }
 }
