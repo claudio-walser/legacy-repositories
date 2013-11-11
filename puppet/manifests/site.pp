322ac898642fe56257f64287ec6d0b8e0dde3b00
Claudio Walser <claudio.walser@srf.ch>
2013-11-12 00:49:04 +0100

    New Hosts and Defaults for Exec Ressource

--- puppet/manifests/site.pp
+++ puppet/manifests/site.pp
@@ -1,3 +1,9 @@
+# Defaults
+Exec {
+	path => "/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin"
+}
+
+# configs and node definitions
 import 'config.pp'
 import 'network.pp'
 import 'nodes.pp'
