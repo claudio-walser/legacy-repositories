9efb4bef993392e8b377c44b35832a9a51cce168
Claudio Walser <claudio.walser@srf.ch>
2013-11-14 22:56:47 +0100

    Puppetmaster - symlink catalog instead of rsync it, script name is wrong now

--- puppet/modules/services/service-puppetmaster/files/etc/puppet/rsync.sh
+++ puppet/modules/services/service-puppetmaster/files/etc/puppet/rsync.sh
@@ -1,8 +1,13 @@
 #!/bin/bash
 
-/usr/bin/rsync -av /mnt/hgfs/puppet/manifests /etc/puppet;
-/usr/bin/rsync -av /mnt/hgfs/puppet/modules /etc/puppet;
-/usr/bin/rsync -av /mnt/hgfs/puppet/Puppetfile /etc/puppet;
+/bin/rm -rf /etc/puppet/manifests
+/bin/ln -s /mnt/hgfs/puppet/manifests /etc/puppet;
+
+/bin/rm -rf /etc/puppet/modules
+/bin/ln -s /mnt/hgfs/puppet/modules /etc/puppet;
+
+/bin/rm -rf /etc/puppet/Puppetfile
+/bin/ln -s /mnt/hgfs/puppet/Puppetfile /etc/puppet;
 
 cd /etc/puppet/;
 /usr/local/bin/librarian-puppet install --verbose --path modules/forge;
