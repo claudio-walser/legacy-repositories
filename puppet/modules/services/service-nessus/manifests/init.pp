7f5d3fadfb7a34a86c1283f3490794925ff839a3
Claudio Walser <claudio@claudio.dev>
2013-12-03 00:52:44 +0100

    Install nessus with an exec resource

--- puppet/modules/services/service-nessus/manifests/init.pp
+++ puppet/modules/services/service-nessus/manifests/init.pp
@@ -1,8 +1,10 @@
 class service-nessus {
 
-	package { 'nessus' :
-		ensure => 'installed',
-		source => '/mnt/backup/development.claudio.dev/scan/backup/files/packages/Nessus-5.2.4-debian6_amd64.deb',
+	# 40FD-395C-19F6-2B1A-0537
+	exec { 'nessus-installation' :
+		command => 'dpkg -i /mnt/backup/development.claudio.dev/scan/backup/files/packages/Nessus-5.2.4-debian6_amd64.deb',
+		path => '/usr/bin',
+		creates => '/etc/init.d/nessusd',
 		require => [
 			Exec['backup-mount']
 		]
