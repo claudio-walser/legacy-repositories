0307819ff8c74d4d2825e0ae27e63361c3406d15
Claudio Walser <claudio@claudio.dev>
2014-05-20 06:19:15 +0200

    Is last node function

--- puppet/modules/roles/role/manifests/mysql.pp
+++ puppet/modules/roles/role/manifests/mysql.pp
@@ -1,3 +1,15 @@
-class role::mysql {
-	
+class role::mysql (
+	$root_password = '1234'
+) {
+
+	if $node_number == 1 {
+		$master = true
+	} else {
+		$master = false
+	}
+
+	class { '::service-percona':
+		root_password => $root_password,
+		master => $master
+	}
 }
