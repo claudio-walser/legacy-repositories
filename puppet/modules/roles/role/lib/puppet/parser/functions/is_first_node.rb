69f51aeaeffe11a4f90c8baf9f43acb311ab96ed
Claudio Walser <claudio@claudio.dev>
2014-05-21 21:09:27 +0200

    Init.pp to server.pp and is_first_node as a function as well

---
+++ puppet/modules/roles/role/lib/puppet/parser/functions/is_first_node.rb
@@ -0,0 +1,20 @@
+module Puppet::Parser::Functions
+	newfunction(:is_first_node, :type => :rvalue) do |args|
+
+		if File.exists?("#{Puppet[:confdir]}/puppetdb.conf") then
+			Puppet::Parser::Functions.autoloader.loadall
+			role = args[0]
+			hostname = args[1]
+
+			query = "hostname~\"#{role}.*\""
+			nodes = function_query_nodes( [query, 'hostname'] )
+			candidates = nodes.select{|hostname| hostname =~  /^#{role}-\d+$/ or hostname == role}
+
+			return hostname == candidates.sort.first
+		else
+			return false
+		end
+
+	end
+
+end
