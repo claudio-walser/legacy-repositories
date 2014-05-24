4842207a848162c2d67d655f0557e0f353a79e7d
Claudio Walser <claudio@claudio.dev>
2014-05-24 10:40:07 +0200

    Role creates vhosts and git repos now from hiera

--- puppet/modules/roles/role/manifests/web.pp
+++ puppet/modules/roles/role/manifests/web.pp
@@ -1,11 +1,24 @@
-class role::web {
-	class{ '::service-nginx':
-		git => true
-	}
-	include ::service-nginx::php
+class role::web (
+	$vhosts = {},
+	$gitRepositories = {}
+) {
 	
-	# load project spaf onto the webservers
-	include ::role::web::spaf
-	include ::role::web::phpdocumentor
-	include ::role::web::test
+	# install git to checkout repos
+	#package {'git':
+	#	ensure => 'installed'
+	#}
+	
+	# install nginx service with php
+	::service-nginx::server{$::fqdn: }
+	::service-nginx::php {$::fqdn: }
+	
+	## create vhosts from hiera	
+	if is_hash($vhosts) {
+		create_resources( '::service-nginx::vhost', $vhosts)
+	}
+
+	if is_hash($gitRepositories) {
+		create_resources( '::service-git::repository', $gitRepositories)
+	}	
+
 }
