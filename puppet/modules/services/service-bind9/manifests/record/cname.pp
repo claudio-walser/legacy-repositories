a7142b7099944431799a0f02b77abd675cc1f845
Claudio Walser <claudio@claudio.dev>
2014-05-24 10:42:21 +0200

    Fixed dns cnames with the right params

--- puppet/modules/services/service-bind9/manifests/record/cname.pp
+++ puppet/modules/services/service-bind9/manifests/record/cname.pp
@@ -1,13 +1,13 @@
 define service-bind9::record::cname (
 	$hostsfile = "/var/lib/bind/$::domain.hosts",
-	$targethost = $::hostname,
-	$ip = $::ipaddress_eth1
+	$host = $::hostname,
+	$cname = $::ipaddress_eth1
 ) {
 
 	
 	concat::fragment { "service-bind9::record::cname-${name}":
 		target  => $hostsfile,
-		content => "${name}				CNAME		${targethost}",
+		content => "${cname}				CNAME		${host}",
 		order   => '06'
 	}
 }
