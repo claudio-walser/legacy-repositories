2a52a00ee193722dcbf6f69f99aa24738d9e1dd0
Claudio Walser <claudio@claudio.dev>
2014-12-15 23:34:28 +0100

    refreshonly and dependencies

--- manifests/init.pp
+++ manifests/init.pp
@@ -20,6 +20,10 @@ class gitlab (
 		ensure => directory,
 	}
 
+	file { '/opt/gitlab/scripts':
+		ensure => directory,
+	}
+
 	wget::fetch { 'download-gitlab-deb-package':
 		source      => 'https://downloads-packages.s3.amazonaws.com/debian-7.6/gitlab_7.5.3-omnibus.5.2.1.ci-1_amd64.deb',
 		destination => '/opt/gitlab/gitlab_7.5.3-omnibus.5.2.1.ci-1_amd64.deb',
@@ -42,19 +46,18 @@ class gitlab (
 
 	file { '/opt/gitlab/scripts/restore.sh':
 		ensure => file,
-		content => template("${module_name}/opt/gitlab/scripts/restore.sh.erb")
+		content => template("${module_name}/opt/gitlab/scripts/restore.sh.erb"),
+		require => File['/opt/gitlab/scripts']
 	}
-
 
 	# reconfigure and restart only on refresh
 	exec { 'gitlab-reconfigure':
 		command	=> '/usr/bin/gitlab-ctl reconfigure',
-		onlyrefresh => true,
+		refreshonly => true,
 		notify => Exec['gitlab-restart']
 	}
-
 	exec { 'gitlab-restart':
 		command	=> '/usr/bin/gitlab-ctl restart',
-		onlyrefresh => true
+		refreshonly => true
 	}
 }
