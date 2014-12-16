cd4cc5e7c78efc23a3e58a94fdd92b0212868953
Claudio Walser <claudio@claudio.dev>
2014-12-16 03:20:45 +0100

    First step for debian/centos

--- manifests/init.pp
+++ manifests/init.pp
@@ -13,30 +13,26 @@ class gitlab (
 		ensure => installed,
 	}
 
-	file { '/opt/gitlab':
+	file { {
+		'/opt/gitlab',
+		'/opt/gitlab/source',
+		'/opt/gitlab/scripts'
+	]:
 		ensure => directory,
 	}
 
-	file { '/opt/gitlab/source':
-		ensure => directory,
-	}
+	case $::operatingsystem {
+		'Debian', 'Ubuntu' : {
+			class { 'gitlab::install::debian': }
+		}
 
-	file { '/opt/gitlab/scripts':
-		ensure => directory,
-	}
+		'RedHat', 'CentOS', 'Fedora' : {
+			class { 'gitlab::install::centos': }
+		}
 
-	wget::fetch { 'download-gitlab-deb-package':
-		source      => 'https://downloads-packages.s3.amazonaws.com/debian-7.6/gitlab_7.5.3-omnibus.5.2.1.ci-1_amd64.deb',
-		destination => '/opt/gitlab/gitlab_7.5.3-omnibus.5.2.1.ci-1_amd64.deb',
-		timeout     => 0,
-		verbose     => true,
-		require 	=> File['/opt/gitlab/source']
-	}
-
-	exec { 'gitlab-install':
-		command	=> '/usr/bin/dpkg -i /opt/gitlab/gitlab_7.5.3-omnibus.5.2.1.ci-1_amd64.deb',
-		require	=> Wget::Fetch['download-gitlab-deb-package'],
-		onlyif => "/usr/bin/apt-show-versions gitlab | /bin/grep 'not installed'"
+		default : {
+			fail("Operatingsystem ${::operatingsystem} not supported")
+		}
 	}
 
 	file { '/etc/gitlab/gitlab.rb':
