7a8016bea53c71bbac4a41d15b70a5b50c93f63e
Claudio Walser <claudio@claudio.dev>
2014-12-18 19:10:19 +0100

    Check if role exists

--- puppet/manifests/site.pp
+++ puppet/manifests/site.pp
@@ -6,16 +6,15 @@ Exec {
 
 ### default node handles everything by hostname
 node default {
-	# include default profile
-	include role::minimal
-
 	# fetch node role and node number out of hostname. Match example:
 	# (anything-but-last-one-or-two-numbers-is-the-role)-01
 	$node_role = get_node_role($hostname)
-	if $node_role {
-		$node_number = get_node_number($hostname)
-		#if defined(Class["role::$node_role"]) {
-			include "role::$node_role"
-		#}
+	$node_number = get_node_number($hostname)
+
+	# include default profile
+	include role::minimal
+
+	if $node_role and defined("role::${node_role}") {
+		include "role::${node_role}"
 	}
 }
