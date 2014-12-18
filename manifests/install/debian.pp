9d5435b74c92d982eb21e09e867665476912a758
Claudio Walser <claudio@claudio.dev>
2014-12-18 07:52:36 +0100

    Added requirements for installation

--- manifests/install/debian.pp
+++ manifests/install/debian.pp
@@ -4,7 +4,8 @@ class gitlab::install::debian {
 	$dependencies = [
 		'openssh-server',
 		'postfix',
-		'wget'
+		'wget',
+		'apt-show-versions'
 	]
 	ensure_resource('package', $dependencies, {
 		ensure => installed
@@ -21,7 +22,8 @@ class gitlab::install::debian {
 	exec { 'gitlab-install':
 		command	=> '/usr/bin/dpkg -i /opt/gitlab/gitlab_7.5.3-omnibus.5.2.1.ci-1_amd64.deb',
 		require	=> Wget::Fetch['download-gitlab-deb-package'],
-		onlyif => "/usr/bin/apt-show-versions gitlab | /bin/grep 'not installed'"
+		onlyif => "/usr/bin/apt-show-versions gitlab | /bin/grep 'not installed'",
+		require => Package['apt-show-versions']
 	}
 
 }
