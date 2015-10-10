1d516381f872d46aca7ae8577d6e25f6d6009002
Claudio Walser <claudio.walser@srf.ch>
2015-10-10 17:43:14 +0200

    Write vmx configuration file, THIS IS UGLY SHIT

--- lib/hypervisor/VmwareWorkstation.py
+++ lib/hypervisor/VmwareWorkstation.py
@@ -2,31 +2,12 @@
 
 import os
 import subprocess
+from string import Template
 from lib.hypervisor.DefaultHypervisor import DefaultHypervisor
 
 class VmwareWorkstation(DefaultHypervisor):
 
-  #vmware
-  #vmwarectrl
-  #vmware-fuseUI
-  #vmware-gksu
-  #vmware-hostd
-  #vmware-installer
-  #vmware-license-check.sh
-  #vmware-license-enter.sh
-  #vmware-modconfig
-  #vmware-mount
-  #vmware-netcfg
-  #vmware-networks
-  #vmware-ping
-  #vmware-tray
-  #vmware-uninstall
-  #vmware-unity-helper
-  #vmware-usbarbitrator
-  #vmware-vdiskmanager
-  #vmware-vim-cmd
-  #vmware-vprobe
-  #vmware-wssc-adminTool
+
 
   # abstract methods to be overwritten in concrete hypervisors
   def hasConfigFile(self, box):
@@ -34,6 +15,104 @@ class VmwareWorkstation(DefaultHypervisor):
 
   def createConfigFile(self, box):
     if not self.hasConfigFile(box):
+      # need to beautify this
+      s = Template("""#!/usr/bin/vmware
+.encoding = "UTF-8"
+
+msg.autoAnswer = "true"
+guestOS = "debian7-64"
+config.version = "8"
+virtualHW.version = "9"
+machine.id = "$fqdn"
+
+numvcpus = "$cpu"
+memsize = "$memory"
+displayName = "$fqdn"
+extendedConfigFile = "$extendedConfigFile"
+
+scsi0.present = "true"
+scsi0.sharedBus = "none"
+scsi0.virtualDev = "lsilogic"
+scsi0:0.present = "true"
+scsi0:0.fileName = "$diskFile"
+scsi0:0.deviceType = "disk"
+
+ide0:0.present = "true"
+ide0:0.deviceType = "cdrom-image"
+ide0:0.startConnected = "true"
+ide0:0.autodetect = "true"
+ide0:0.fileName = "$installMedium"
+
+# shared folders
+sharedFolder.maxNum = "$numSharedFolders"
+isolation.tools.hgfs.disable = "false"
+"""
+      )
+  
+      substitutedConfig = s.substitute(
+        fqdn =box.getFQDN(),
+        cpu = box.getHardwareCpu(),
+        memory = box.getHardwareMemory(),
+        extendedConfigFile = self.__getConfigPath(box) + "f",
+        diskFile = self.__getDiskPath(box),
+        installMedium = box.getInstallMedium(),
+        numSharedFolders = len(box.getSharedFolders())
+      )
+
+      # find out how to do this in string.Template
+      # append shared folders
+      i = 0
+      for sharedFolder in box.getSharedFolders():
+        if sharedFolder[:1] == '.':
+          sharedFolder = os.getcwd() + sharedFolder[1:]
+          guestName = sharedFolder.split("/").pop()
+        sharedFolderTemplate = Template("""
+sharedFolder$i.present = "true"
+sharedFolder$i.enabled = "true"
+sharedFolder$i.readAccess = "true"
+sharedFolder$i.writeAccess = "true"
+sharedFolder$i.hostPath = "$sharedFolder"
+sharedFolder$i.guestName = "$guestName"
+sharedFolder$i.expiration = "never"
+"""
+        )
+
+        # replace leading dot with current working dir
+        sharedFolderConfig = sharedFolderTemplate.substitute(
+          i = i,
+          sharedFolder = sharedFolder,
+          guestName = guestName
+        )
+        i += 1
+        substitutedConfig += sharedFolderConfig
+        
+
+
+      #substitutedConfig += "\n# network interfaces"
+      i = 0
+      for interface in box.getNetworkInterfaces():
+        
+        networkTemplate = Template("""
+ethernet$i.present = "true"
+ethernet$i.connectionType = "bridged"
+ethernet$i.generatedAddressOffset = "$offset"
+ethernet$i.wakeOnPcktRcv = "false"
+ethernet$i.addressType = "generated"
+ethernet$i.pciSlotNumber = "3$i"
+"""
+        )
+
+        # replace leading dot with current working dir
+        networkConfig = networkTemplate.substitute(
+          i = i,
+          offset = i * 10
+        )
+        i += 1
+        substitutedConfig += networkConfig
+
+      with open(self.__getConfigPath(box), "w") as configFile:
+        configFile.write("%s" % substitutedConfig)
+      
       print('write vmx Config file')
 
   def hasDisk(self, box):
@@ -56,13 +135,11 @@ class VmwareWorkstation(DefaultHypervisor):
 
       processOutput = subprocess.check_output(command)
 
-
-
   def isRegistered(self, box):
     raise NotImplementedError("Not able to fetch registered vm's in vmware-workstation")
 
   def register(self, box):
-    raise NotImplementedError("Not able to register vm in vmware-workstation")
+    raise NotImplementedError("Not able to register a vm in vmware-workstation")
 
   def isRunning(self, box):
     command = [
