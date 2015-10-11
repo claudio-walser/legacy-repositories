05a249876d7890f9f86fa82fa3ec7e2ab2849aa8
Claudio Walser <claudio.walser@srf.ch>
2015-10-11 20:21:21 +0200

    Abstract methods only whats used in lib/Knack.py, consider everything else as hypervisor internals

--- lib/hypervisor/DefaultHypervisor.py
+++ lib/hypervisor/DefaultHypervisor.py
@@ -2,29 +2,7 @@
 
 class DefaultHypervisor(object):
 
-
   # abstract methods to be overwritten in concrete hypervisors
-  def hasConfigFile(self, box):
-    raise NotImplementedError( "Should have implemented this" )
-
-  def createConfigFile(self, box):
-   raise NotImplementedError( "Should have implemented this" )
-
-  def hasDisk(self, box):
-    raise NotImplementedError( "Should have implemented this" )
-
-  def createDisk(self, box):
-    raise NotImplementedError( "Should have implemented this" )
-
-  def isRegistered(self, box):
-    raise NotImplementedError( "Should have implemented this" )
-
-  def register(self, box):
-    raise NotImplementedError( "Should have implemented this" )
-
-  def isRunning(self, box):
-    raise NotImplementedError( "Should have implemented this" )
-
   def start(self, box):
     raise NotImplementedError( "Should have implemented this" )
 
@@ -32,4 +10,16 @@ class DefaultHypervisor(object):
     raise NotImplementedError( "Should have implemented this" )
 
   def restart(self, box):
+    raise NotImplementedError( "Should have implemented this" )
+
+  def destroy(self, box):
+    raise NotImplementedError( "Should have implemented this" )
+
+  def isCreated(self, box):
+   raise NotImplementedError( "Should have implemented this" )
+
+  def isRunning(self, box):
+    raise NotImplementedError( "Should have implemented this" )
+
+  def isInstalled(self, box):
     raise NotImplementedError( "Should have implemented this" )
